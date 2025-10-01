6b5d45eb433cebc5c2e0c20c8dcd381ffd981802
mohamedelhadad-22 <mohamedg.elhadad@gmail.com>
2025-10-01 20:48:26 +0300

    -- Add new view components for various sections and remove outdated footer components
    -- add new sections to about us view componen

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -142,6 +142,204 @@ export default defineComponent({
         </div>
       </div>
     </div>
+
+    <div class="valuesAccordion">
+      <div class="valuesAccordion__inner">
+        <div class="valuesAccordion__tiles">
+          <div class="valuesAccordion__tile">
+            <div class="valuesAccordion__tileContent">
+              <div
+                class="valuesAccordion__decor label label--decor label--decor03"
+              ></div>
+              <h3 class="valuesAccordion__tileTitle heading__h3">
+                Our Mission
+              </h3>
+              <p class="valuesAccordion__tileText p margin-2-t">
+                To deliver flexible and reliable solutions for our customers and
+                create value for our shareholders while supporting the
+                development of the logistics ecosystem within Kingdom Saudi
+                Arabia and across the region.
+              </p>
+            </div>
+            <div class="valuesAccordion__tileImage">
+              <img
+                class="valuesAccordion__tileImg"
+                src="https://folkmaritime.com/storage/media/147/conversions/Untitled-5121212.medium.jpg"
+                alt="image"
+              />
+            </div>
+          </div>
+
+          <div class="valuesAccordion__tile">
+            <div class="valuesAccordion__tileContent">
+              <div
+                class="valuesAccordion__decor label label--decor label--decor04"
+              ></div>
+              <h3 class="valuesAccordion__tileTitle heading__h3">Our Vision</h3>
+              <p class="valuesAccordion__tileText p margin-2-t">
+                To be the leading liner and feeder operator in the Middle East
+                and the surrounding region, further strengthening the Kingdom of
+                Saudi Arabia’s position as a global logistics hub.
+              </p>
+            </div>
+            <div class="valuesAccordion__tileImage">
+              <img
+                class="valuesAccordion__tileImg"
+                src="https://folkmaritime.com/storage/media/120/conversions/WhatsApp-Image-1446-10-29-at-15-06-28.medium.jpg"
+                alt="image"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="valuesAccordion__content">
+          <div class="valuesAccordion__image">
+            <img
+              class="valuesAccordion__img"
+              style="opacity: 1; z-index: 2"
+              src="https://folkmaritime.com/storage/media/159/conversions/11-02.medium.jpg"
+              alt="image"
+              data-values-accordion-image="0"
+            />
+            <img
+              class="valuesAccordion__img"
+              style="z-index: 1; opacity: 1"
+              src="https://folkmaritime.com/storage/media/160/conversions/222222222222222-01.medium.jpg"
+              alt="image"
+              data-values-accordion-image="1"
+            />
+            <img
+              class="valuesAccordion__img"
+              style="z-index: 1; opacity: 1"
+              src="https://folkmaritime.com/storage/media/161/conversions/33333333333333-02.medium.jpg"
+              alt="image"
+              data-values-accordion-image="2"
+            />
+            <img
+              class="valuesAccordion__img"
+              style="z-index: 1; opacity: 1"
+              src="https://folkmaritime.com/storage/media/162/conversions/4445444-01.medium.jpg"
+              alt="image"
+              data-values-accordion-image="3"
+            />
+          </div>
+
+          <div class="valuesAccordion__accordion">
+            <div
+              class="valuesAccordion__decor label label--decor label--decor03"
+            ></div>
+            <h3 class="valuesAccordion__title heading__h3">Our Values</h3>
+            <p class="valuesAccordion__text p margin-2-t">
+              Folk Maritime is a dynamic, innovative, and reliable logistics
+              company committed to advancing Saudi Arabia’s transportation and
+              logistics ambitions through continuous progress and consistent
+              delivery.
+            </p>
+
+            <div class="valuesAccordion__items margin-6-t">
+              <div
+                class="valuesAccordion__item valuesAccordion__item--opened"
+                data-values-accordion-item="0"
+              >
+                <button
+                  type="button"
+                  class="valuesAccordion__itemHead"
+                  data-values-accordion-head=""
+                >
+                  <span class="valuesAccordion__itemTitle"
+                    >Customer Focus | Connecting Your Focus to Customer
+                    Success<span> </span
+                  ></span>
+                </button>
+                <div
+                  class="valuesAccordion__itemBody"
+                  data-values-accordion-body=""
+                  style=""
+                >
+                  <div class="valuesAccordion__itemBodyInner p">
+                    We prioritize our customers' needs, ensuring we meet their
+                    expectations through a balance of personal attention and
+                    efficient technology, striving to build strong, lasting
+                    relationships.
+                  </div>
+                </div>
+              </div>
+              <div class="valuesAccordion__item" data-values-accordion-item="1">
+                <button
+                  type="button"
+                  class="valuesAccordion__itemHead"
+                  data-values-accordion-head=""
+                >
+                  <span class="valuesAccordion__itemTitle"
+                    >Integrity | Building Bridges of Trust: Integrity in Every
+                    Action<span> </span
+                  ></span>
+                </button>
+                <div
+                  class="valuesAccordion__itemBody"
+                  data-values-accordion-body=""
+                  style=""
+                >
+                  <div class="valuesAccordion__itemBodyInner p">
+                    We are committed to transparency, honesty, and
+                    accountability, striving to make ethical decisions that
+                    positively impact our stakeholders, our community, and the
+                    world around us
+                  </div>
+                </div>
+              </div>
+              <div class="valuesAccordion__item" data-values-accordion-item="2">
+                <button
+                  type="button"
+                  class="valuesAccordion__itemHead"
+                  data-values-accordion-head=""
+                >
+                  <span class="valuesAccordion__itemTitle"
+                    >Innovation | Driving Excellence: Innovation, our north
+                    star<span> </span
+                  ></span>
+                </button>
+                <div
+                  class="valuesAccordion__itemBody"
+                  data-values-accordion-body=""
+                  style=""
+                >
+                  <div class="valuesAccordion__itemBodyInner p">
+                    Experience the future of ship management and service with
+                    our advanced IoT and technology solutions, delivering cost
+                    efficiency and innovative shipping solutions to the benefit
+                    of our costumers.
+                  </div>
+                </div>
+              </div>
+              <div class="valuesAccordion__item" data-values-accordion-item="3">
+                <button
+                  type="button"
+                  class="valuesAccordion__itemHead"
+                  data-values-accordion-head=""
+                >
+                  <span class="valuesAccordion__itemTitle"
+                    >Sustainability | Sailing Towards Tomorrow: Sustainability
+                    in Action<span> </span
+                  ></span>
+                </button>
+                <div
+                  class="valuesAccordion__itemBody"
+                  data-values-accordion-body=""
+                  style=""
+                >
+                  <div class="valuesAccordion__itemBodyInner p">
+                    Dedicated to minimizing our environmental impact through
+                    fuel-efficient vessels and cleaner operations, extending
+                    this commitment to sustainable practices in our workplaces.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -420,5 +618,281 @@ export default defineComponent({
   background-color: #004d4514;
   width: 5rem;
   height: 5rem;
+}
+
+@media (min-width: 768px) {
+  .valuesAccordion {
+    margin-inline: 0px;
+  }
+}
+.valuesAccordion__inner {
+  padding-block: clamp(64px, 9.2903225806vw, 144px);
+  background-color: #004d45;
+}
+@media (min-width: 768px) {
+  .valuesAccordion__inner {
+    padding-inline: 5.42%;
+  }
+}
+
+@media (min-width: 768px) {
+  .valuesAccordion__tile {
+    grid-template-columns: 40.424% 40.424%;
+    column-gap: 12.764%;
+  }
+}
+.valuesAccordion__tile {
+  display: grid;
+  grid-template-rows: auto auto 1fr;
+  width: 100%;
+}
+
+.valuesAccordion__tileContent {
+  position: relative;
+  /* grid-row: 2 / 3;
+  padding-inline-start: 4.2rem; */
+}
+@media (min-width: 768px) {
+  .valuesAccordion__tileContent {
+    padding-inline-start: 15.788%;
+    grid-row: 1 / 2;
+    grid-column: 1 / 2;
+  }
+}
+.valuesAccordion__tileImage {
+  height: max(190px, min(24.5161290323vw, 380px));
+  height: clamp(190px, 24.5161290323vw, 380px);
+  position: relative;
+  grid-row: 1 / 2;
+  margin-bottom: 3.4rem;
+}
+@media (min-width: 768px) {
+  .valuesAccordion__tileImage {
+    grid-row: 1 / 2;
+    grid-column: 2 / 3;
+    margin-bottom: 0;
+  }
+}
+.valuesAccordion__decor {
+  position: absolute;
+  top: 0.5rem;
+  inset-inline-start: 0;
+}
+.label {
+  display: inline-flex;
+  align-items: center;
+  text-transform: uppercase;
+  line-height: 1.25;
+  font-size: 1.6rem;
+  font-weight: 500;
+}
+.valuesAccordion__tileTitle {
+  line-height: 1.2;
+  color: #80fd66;
+  font-weight: 300;
+}
+@media (min-width: 768px) {
+  .valuesAccordion__tileText {
+    font-size: 1.8rem;
+  }
+}
+.valuesAccordion__tileText {
+  color: #fff;
+}
+.p {
+  line-height: 1.4;
+  font-size: 1.8rem;
+  font-weight: 400;
+}
+.margin-2-t {
+  margin-top: 16px;
+}
+.valuesAccordion__tileImg {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.valuesAccordion__tiles {
+  gap: clamp(60px, 6.1935483871vw, 96px);
+  display: flex;
+  flex-wrap: wrap;
+}
+.valuesAccordion__content {
+  display: flex;
+  flex-direction: column;
+  margin-top: 7.6rem;
+}
+
+@media (min-width: 768px) {
+  .valuesAccordion__content {
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+    margin-top: 14rem;
+  }
+}
+.valuesAccordion__image {
+  display: block;
+  position: relative;
+  margin-bottom: 3.2rem;
+  width: 100%;
+  height: 32rem;
+  overflow: hidden;
+}
+@media (min-width: 768px) {
+  .valuesAccordion__image {
+    width: 44%;
+    height: 52rem;
+  }
+}
+
+.valuesAccordion__img {
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.valuesAccordion__accordion {
+  position: relative;
+  width: 100%;
+}
+@media (min-width: 768px) {
+  .valuesAccordion__accordion {
+    padding-inline-start: 8.5rem;
+    width: 50%;
+  }
+}
+.valuesAccordion__decor {
+  position: absolute;
+  top: 0.5rem;
+  inset-inline-start: 0;
+}
+.label {
+  display: inline-flex;
+  align-items: center;
+  text-transform: uppercase;
+  line-height: 1.25;
+  font-size: 1.6rem;
+  font-weight: 500;
+}
+.valuesAccordion__title {
+  padding-inline-start: 4.2rem;
+  line-height: 1.2;
+  color: #80fd66;
+  font-weight: 300;
+}
+
+@media (min-width: 768px) {
+  .valuesAccordion__title {
+    padding-inline-start: 0;
+  }
+}
+
+.valuesAccordion__text {
+  color: #fff;
+  padding-inline-start: 4.2rem;
+}
+.p {
+  line-height: 1.4;
+  font-size: 1.8rem;
+  font-weight: 400;
+}
+.margin-2-t {
+  margin-top: 16px;
+}
+@media (min-width: 768px) {
+  .valuesAccordion__text {
+    padding-inline-start: 0;
+  }
+}
+.valuesAccordion__items {
+  border-top: 1px solid #80fd66;
+  border-bottom: 1px solid #80fd66;
+  counter-reset: ac-item;
+}
+.margin-6-t {
+  margin-top: 48px;
+}
+.valuesAccordion__itemHead {
+  min-height: max(30px, min(2.1935483871vw, 34px));
+  min-height: clamp(30px, 2.1935483871vw, 34px);
+  display: flex;
+  position: relative;
+  align-items: center;
+  background-color: transparent;
+  cursor: pointer;
+  padding: 3rem;
+  width: 100%;
+  color: #fff;
+}
+button {
+  border: 0;
+}
+@media (min-width: 768px) {
+  .valuesAccordion__itemTitle {
+    font-size: 2.4rem;
+  }
+}
+.valuesAccordion__itemTitle {
+  transition: color 0.3s;
+  line-height: 1.4;
+  font-size: 2rem;
+  font-weight: 300;
+}
+
+.valuesAccordion__itemHead:after {
+  font-family: icomoon !important;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 400;
+  line-height: 1;
+  text-transform: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: block;
+  position: absolute;
+  top: 50%;
+  width: 24px;
+  height: 24px;
+  margin-top: -12px;
+  font-size: 24px;
+  line-height: 24px;
+  text-align: right;
+  transition: color 0.3s;
+  color: #80fd66;
+  content: "";
+}
+html:not([dir="rtl"]) .valuesAccordion__itemHead:after {
+  right: 2rem;
+}
+html:not([dir="rtl"]) .valuesAccordion__itemHead:after {
+  right: 0;
+}
+.valuesAccordion__item--opened .valuesAccordion__itemHead:after {
+  content: "";
+}
+.valuesAccordion__item--opened .valuesAccordion__itemBody {
+  height: auto;
+  overflow: visible;
+}
+.valuesAccordion__itemBody {
+  display: block;
+  height: 0;
+  overflow: hidden;
+}
+.valuesAccordion__itemBodyInner {
+  padding-top: 1.2rem;
+  padding-bottom: 3rem;
+  padding-inline: 3rem;
+  line-height: 1.4;
+  color: #fff;
+  font-size: 1.8rem;
+  font-weight: 300;
+  font-style: normal;
 }
 </style>
